4678c014d1b8a58ba4ea87e6cbb3b3e74f019815
Pablo Aldana <pabloaldana.cipo@gmail.com>
2024-05-01 11:25:51 -0300

    mepeo de las bandas usando fecht

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,26 @@
+import React, { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 function Home() {
+  const [bandas, setBandas] = useState([]);
+
+  useEffect(() => {
+    const fetchBandas = async () => {
+      try {
+        const response = await fetch("/mocks/apiBandas.json");
+        const data = await response.json();
+        setBandas(data);
+      } catch (error) {
+        console.error("Error fetching bandas:", error);
+      }
+    };
+
+    fetchBandas();
+  }, []);
+  console.log("Datos de bandas:", bandas);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -11,8 +29,10 @@ function Home() {
         className="flex-grow bg-cover bg-center"
         style={{ backgroundImage: "url(images/fondo-negro.jpg)" }}
       >
-        <div className="mb-6">
-          <Card />
+        <div className="flex flex-wrap justify-center gap-4 p-6">
+          {bandas.map((banda) => (
+            <Card key={banda.id} banda={banda} />
+          ))}
         </div>
       </div>
       <Footer />
